c30b3f30eba2fb0d7da06492ad7d477067d408ec
Michael Grauer <michael.grauer@kitware.com>
2012-03-22 16:20:48 -0400

    ENH: first commit, added skeleton of plugin.

---
+++ controllers/IndexController.php
@@ -0,0 +1,38 @@
+<?php
+/*=========================================================================
+ MIDAS Server
+ Copyright (c) Kitware SAS. 26 rue Louis Guérin. 69100 Villeurbanne, FRANCE
+ All rights reserved.
+ More information http://www.kitware.com
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+         http://www.apache.org/licenses/LICENSE-2.0.txt
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+=========================================================================*/
+/** demo controller*/
+class Challenge_IndexController extends Challenge_AppController
+{
+
+  /**
+   * @method initAction()
+   *  Index Action (first action when we access the application)
+   */
+  function init()
+    {
+
+    } // end method indexAction
+
+  /** index action*/
+  function indexAction()
+    {  
+    }
+
+}//end class
