8df15f0b71c6709ef5739f02662e2011e1235651
Yuzheng Zhou <yuzheng.zhou@kitware.com>
2012-04-18 13:25:43 -0400

    ENH: added scope in js files

--- public/js/competitor/competitor.showscore.js
+++ public/js/competitor/competitor.showscore.js
@@ -2,6 +2,48 @@ var midas = midas || {};
 midas.challenge = midas.challenge || {};
 midas.challenge.competitor = midas.challenge.competitor || {};
 
+midas.challenge.competitor.showscoreSetup = function () {
+  if(json.challengeId) {
+      var aniSpeed = 2000;   
+      if(json.processingComplete !== 'true') { 
+        $('div#midas_challenge_competitor_listScoreStatus').html("Calculation status: not complete. It will be automatically updated shortly.");
+        $('div#midas_challenge_competitor_listScoreStatus').animate( { color: 'red' }, aniSpeed);
+        }
+      else {
+        $('div#midas_challenge_competitor_listScoreStatus').html("Calculation status: complete."); 
+        $('div#midas_challenge_competitor_listScoreStatus').animate( { color: 'green' }, aniSpeed);
+        }  
+      }
+   
+    $("div.scoreHeading").qtip({
+          content: 'Click to show or hide scores for this challenge!',
+          show: 'mouseover',
+          hide: 'mouseout',
+          position: {
+                target: 'mouse',
+                my: 'bottom left',
+                viewport: $(window), // Keep the qtip on-screen at all times
+                effect: true // Disable positioning animation
+             }
+         }).click(function(){
+             $(this).next(".scoreContent").slideToggle(100);
+            }) 
+            
+    .toggle(
+      function() 
+        {
+        $(this).children("span").text("+");
+        }, 
+      function()
+        {
+        $(this).children("span").text("-");
+        }
+     );           
+}
+
+
+
+
 $(document).ready(function() 
   { 
     
@@ -19,32 +61,7 @@ $(document).ready(function()
         
     $(".scoreContent").show();
     
-    if(json.challengeId)
-      {
-      if(json.processingComplete !== 'true') 
-        { 
-        $('div#midas_challenge_competitor_listScoreStatus').html("Calculation status: not complete.");
-        }
-      else
-        {
-        $('div#midas_challenge_competitor_listScoreStatus').html("Calculation status: complete.");  
-        }
-      }
-      
-    $(".scoreHeading").click(function(){
-    $(this).next(".scoreContent").slideToggle(100);
-     })
-     
-    .toggle(
-      function() 
-        {
-        $(this).children("span").text("+");
-        }, 
-      function()
-        {
-        $(this).children("span").text("-");
-        }
-     );      
+    midas.challenge.competitor.showscoreSetup();
 
   });
  
